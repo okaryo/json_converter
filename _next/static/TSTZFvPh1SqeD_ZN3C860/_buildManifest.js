b195706c5831db95c8d14a7cb80299a8a9af29cf
okaryo <okaryo@users.noreply.github.com>
2022-03-20 15:14:27 +0000

    deploy: 7ae7e8ba42fabc76d593369b4f7f56a6f383ae3e

--- _next/static/TSTZFvPh1SqeD_ZN3C860/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/756-5bc5719665f70d8e.js","static/chunks/pages/index-1efe6625b677ee3b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
