275e6e33fb1b4e2a71f2fd40a522f3b7b0634d83
okaryo <okaryotr@gmail.com>
2022-03-20 18:02:10 +0900

    [add] redux package

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Provider, useStore } from 'react-redux'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const store = useStore()
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
+
+export default MyApp
